7bb236ce69c76558c6badd47a83c4d0382b23010
Maria Ortega Rivas <mariorte@c1r6s4.42malaga.com>
2024-12-27 13:16:16 +0100

    Intentar escribir los argumentos de la variatica

--- printf.h
+++ printf.h
@@ -1,33 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   printf.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mariorte <mariorte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/26 16:23:34 by mariorte          #+#    #+#             */
-/*   Updated: 2024/12/27 13:15:20 by mariorte         ###   ########.fr       */
+/*   Created: 2024/12/27 12:59:56 by mariorte          #+#    #+#             */
+/*   Updated: 2024/12/27 13:00:28 by mariorte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "printf.h"
+// Define todas las funciones y estructuras necesarias
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
 
-int ft_printf(char const *ctrl_str, ...)
-{
-	int	c;
-	va_list	args;
-	va_start(args, ctrl_str);
-	
-	c = va_arg(args, int);
-	write(1, c, 1);
 
-	return (1);
-}
-
-int	main(void)
-{
-	int	c;
-
-	c = 'A';
-	ft_printf("Probando", c);
-}
+#endif
